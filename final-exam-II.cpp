375a1005cb36aa6bf01c29cae833bb3c78b4afd3
Annie <annmora21@gmail.com>
2025-12-11 19:21:48 -0800

    Update

---
+++ final-exam-II.cpp
@@ -0,0 +1,22 @@
+// COMSC-210 | Final Exam II | Annie Morales
+// IDE used: Visual Studio Code
+
+#include <iomanip>
+#include <string>
+using namespace std;
+
+struct coffeeBooth
+{
+    string names;
+    string drink;
+
+    string names[] = {"Alex", "Erick" , "Ashley", "Dustin", "Robin", "Steve", "Nancy", "Jane"};
+    string drinks[] = {"Latte", "Cold Brew", "Espresso", "Mocha", "Cappuccino", "Iced Tea"};
+};
+
+
+int main()
+{
+    
+    return 0;
+}
