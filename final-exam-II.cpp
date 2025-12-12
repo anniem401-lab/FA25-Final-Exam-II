5537b467c72f354e5891e53751bf14c2f83143d5
Annie <annmora21@gmail.com>
2025-12-11 20:16:48 -0800

    Milestone 1

--- final-exam-II.cpp
+++ final-exam-II.cpp
@@ -8,6 +8,8 @@
 #include <cstdlib>
 using namespace std;
 
+const int customers = 5;
+
 struct Node {
     string name;
     string drink;
@@ -31,7 +33,15 @@ public:
         newNode->drink = drink;
         newNode->next = head;
         head = newNode;
-        
+
+    }
+
+    void displayQueue() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << "Name: " << current->name << " - " << "Drink: " << current->drink << endl;
+            current = current->next;
+        }
     }
 };
 
@@ -41,14 +51,19 @@ int main()
     Node *head = nullptr;
     srand(time(0));
 
-    string names[] = {"Alex", "Erick" , "Ashley", "Dustin", "Robin", "Steve", "Nancy", "Jane"};
+    Queue line;
+
+    string names[] = {"Alex", "Erick" , "Ashley", "Dustin", "Robin", "Steve", "Nancy", "Jane", "Jack", "Lily", "Miri", "Ben"};
     string drinks[] = {"Latte", "Cold Brew", "Espresso", "Mocha", "Cappuccino", "Iced Tea"};
 
     for (int i = 0; i < 5; i++) {
-
+        string aName = names[rand() % 11];
+        string aDrink = drinks[rand() % 6];
+        line.addCust(aName, aDrink);
     }
 
     cout << "\nQueue at Coffee Booth: \n";
+    line.displayQueue();
 
     return 0;
 }
